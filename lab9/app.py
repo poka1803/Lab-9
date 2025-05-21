a223e90e5f8487a396c6dd5a024d98c6d847d844
Khisamova Leyla <khisamova06@internet.ru>
2025-05-21 21:58:54 +0300

    lab9

--- lab9/app.py
+++ lab9/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
